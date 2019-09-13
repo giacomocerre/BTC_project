3ec7c9403fec3847021b3e2530e9ea5336e3f25d
Giacomo Cerrretini <giacomocerrretini@host-131-114-215-254.m.unipi.it>
2019-09-13 14:27:44 +0200

    Add server.js comment

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
+// require library
 var express=require('express');
 var bodyparser=require('body-parser');
 var fs=require('fs');
 var cors=require('cors');
 
 var app=express();
-
+// parser type
 app.use(bodyparser.urlencoded({extended:false}));
 app.use(bodyparser.json());
 app.use(cors())
 
+// PORT DECLARATION
 const PORT = 3030;
 
+// SERVER LISTEN
 var server=app.listen(PORT, function () {
     console.log("Server running on " + PORT)
 });
-
+// FileSystem, parse JSON file
 var data1=fs.readFileSync('./data/vis_final_market_cap(inflated).json', 'utf8');
 var marketCap=JSON.parse(data1);
 
@@ -32,7 +35,7 @@ var minersRev = JSON.parse(data5);
 
 var data6 = fs.readFileSync('./data/vis_final_raee.json', 'utf8');
 var raee = JSON.parse(data6);
-
+// Get call
 app.get('/marketCap', function (req, res) {
     res.json(marketCap)
 });

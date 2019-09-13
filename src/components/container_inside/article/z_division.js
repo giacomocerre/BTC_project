552f172f15b8ec08036e092d15ccb02197425e85
Giacomo Cerrretini <giacomocerrretini@host-131-114-215-254.m.unipi.it>
2019-09-13 12:46:07 +0200

    Edit pro conto click

--- src/components/container_inside/article/z_division.js
+++ src/components/container_inside/article/z_division.js
@@ -1,27 +1,42 @@
 import React from "react"
 
-function Division (pars){
-    if(pars.type === "no"){
-        return(
-            <div id={pars.id} class="division">
-                <h5 class="num_block">Block {pars.n_block}</h5>
-                <hr class="line_open line_left"/>
-                <hr class="line_open line_right"/>
-                <h2 class="block_title">{pars.text}</h2>
-            </div>
-        );
+class Division extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            choose : 0
+        };
+    }
+    
+    ChooseOP(event){
+        const id = event.target.id;
+        console.log(id);
+    }
+
+    render(){
+        if(this.props.type === "no"){
+            return(
+                <div id={this.props.id} class="division">
+                    <h5 class="num_block">Block {this.props.n_block}</h5>
+                    <hr class="line_open line_left"/>
+                    <hr class="line_open line_right"/>
+                    <h2 class="block_title">{this.props.text}</h2>
+                </div>
+            );
         }else{
             return(
-                <div id={pars.id} class="division pro_contro_content">
-                    <div id="pro" class="pro_contro">
-                        <h2 class="block_title">{pars.text_1}</h2>
+                <div id={this.props.id} class="division pro_contro_content">
+                    <div id="pro" class="pro_contro" onClick={this.ChooseOP}>
+                        <h2 class="block_title">{this.props.text_1}</h2>
                     </div>
-                    <div id="contro" class="pro_contro">
-                        <h2 class="block_title">{pars.text_2}</h2>
+                    <div id="contro" class="pro_contro" onClick={this.ChooseOP}>
+                        <h2 class="block_title">{this.props.text_2}</h2>
                     </div>
                 </div>
             )
         }
+    }
+
 }
 
 export default Division

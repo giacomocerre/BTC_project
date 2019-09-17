d543c10b3bb9da3878a8aa7f449ee5a184187af1
Giacomo Cerrretini <giacomocerrretini@host-131-114-215-176.m.unipi.it>
2019-09-17 18:35:56 +0200

    Modfy relation html

--- src/components/open.js
+++ src/components/open.js
@@ -32,6 +32,7 @@ class Open extends React.Component {
       return (
         <section id="open" className="full_height">
             <div id="open_content">
+                
                 <h1 id="main_title">Blockchain: <br/>Il prezzo del consenso</h1>
                 <p className="subtitle">Dolor sit amet der mit losel numn sit amet der fun cus mein.</p>
                 <img id="logo" src="img/logo.png" alt="logo" />
